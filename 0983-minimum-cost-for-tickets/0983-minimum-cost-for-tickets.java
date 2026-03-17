62e9840a63be5d98126a35fdcb37b2572812798a
amandeepyadav8175-debug <amandeepyadav8175@gmail.com>
2026-03-18 00:06:33 +0530

    Time: 2 ms (44.66%), Space: 43.4 MB (57.41%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -6,21 +6,19 @@ class Solution {
         }
         int lastday=days[days.length-1]+30;
         int dp[]=new int[lastday+1];
-        Arrays.fill(dp,-1);
-        return recur(days,costs,days[0],dp);
-       
-        
-    }
-    public int recur(int[] days,int[] costs,int currday,int dp[]){// currdays=i
-        if(currday>days[days.length-1]) return 0;
-        if(dp[currday]!=-1) return dp[currday];
-
-        if(!set.contains(currday)){
-            return dp[currday]= 0+recur(days,costs,currday+1,dp);
+       for(int day=days[days.length-1];day>=0;day--){
+        if(!set.contains(day)){
+             dp[day]= 0+dp[day+1];//recur(days,costs,day+1,dp);
+            continue;
         }
-        int day1=costs[0]+recur(days,costs,currday+1,dp);
-        int day2=costs[1]+recur(days,costs,currday+7,dp);
-        int day3=costs[2]+recur(days,costs,currday+30,dp);
-        return dp[currday]=Math.min(day1,Math.min(day2,day3));
+        int day1=costs[0]+dp[day+1];//recur(days,costs,day+1,dp);
+        int day2=costs[1]+dp[day+7];//recur(days,costs,day+7,dp);
+        int day3=costs[2]+dp[day+30];//recur(days,costs,day+30,dp);
+        dp[day]=Math.min(day1,Math.min(day2,day3));
+
+       }
+        return  dp[days[0]];//recur(days,costs,days[0],dp);
+       
     }
-}
+    }
+   
