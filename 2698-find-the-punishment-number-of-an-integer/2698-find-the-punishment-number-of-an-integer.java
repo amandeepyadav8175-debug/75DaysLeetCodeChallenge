78d89bdc773b465fc40e8558dc98563203184fde
amandeepyadav8175-debug <amandeepyadav8175@gmail.com>
2026-03-19 02:45:46 +0530

    Time: 260 ms (5.71%), Space: 46.4 MB (22.04%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.java
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.java
@@ -1,27 +1,39 @@
 class Solution {
     public int punishmentNumber(int n) {
         int res=0;
-        for(int i=0;i<=n;i++){
+        for(int i=1;i<=n;i++){
             String i2=Integer.toString(i*i);
-            if(ispartition(0,i2,i,0)){// beacuse os loop i beacomes target
+            int dp[][]= new int[i2.length()][i+1];// i beacomes target beacuse os loop
+            for(int j=0;j<i2.length();j++){
+                Arrays.fill(dp[j],-1);
+            }
+            if(ispartition(0,i2,i,0,dp)){// beacuse os loop i beacomes target
                 res+=(i*i);
             }
         }
         return res;
         
     }
-    public boolean ispartition(int partindx,String i2,int target,int currsum){// target=81=8+1=9
+    public boolean ispartition(int partindx,String i2,int target,int currsum,int dp[][]){// target=81=8+1=9
          int n=i2.length();
          if(partindx==n){//100|
-            return (currsum==target);
+            return (currsum==target);// here check condition 
          }
-         if(currsum>target) return false;
+         if(currsum>target){ 
+            return false;// pruning
+
+         }
+         if(dp[partindx][currsum]!=-1){
+            return (dp[partindx][currsum]==1); 
+         }
          for(int indx=partindx;indx<n;indx++){
             int val=Integer.parseInt(i2.substring(partindx,indx+1));
-            if(ispartition(indx+1,i2,target,currsum+val)){
+            if(ispartition(indx+1,i2,target,currsum+val,dp)){
+                dp[partindx][currsum]=1;
                 return true;
             }
          }
+         dp[partindx][currsum]=0;
          return false;
 
     }
