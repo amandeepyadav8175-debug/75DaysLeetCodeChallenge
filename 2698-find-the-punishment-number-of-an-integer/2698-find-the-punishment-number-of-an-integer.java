dc5321b77c25731718f8068bc156d9a66defd709
amandeepyadav8175-debug <amandeepyadav8175@gmail.com>
2026-03-19 02:26:38 +0530

    Time: 148 ms (17.55%), Space: 46.6 MB (8.16%) - LeetHub

---
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int punishmentNumber(int n) {
+        int res=0;
+        for(int i=0;i<=n;i++){
+            String i2=Integer.toString(i*i);
+            if(ispartition(0,i2,i,0)){// beacuse os loop i beacomes target
+                res+=(i*i);
+            }
+        }
+        return res;
+        
+    }
+    public boolean ispartition(int partindx,String i2,int target,int currsum){// target=81=8+1=9
+         int n=i2.length();
+         if(partindx==n){//100|
+            return (currsum==target);
+         }
+         for(int indx=partindx;indx<n;indx++){
+            int val=Integer.parseInt(i2.substring(partindx,indx+1));
+            if(ispartition(indx+1,i2,target,currsum+val)){
+                return true;
+            }
+         }
+         return false;
+
+    }
+}
