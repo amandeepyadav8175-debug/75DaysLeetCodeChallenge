f9e1ce2cf2eff9aa523acfc3d1824d9d128e0e5e
amandeepyadav8175-debug <amandeepyadav8175@gmail.com>
2026-03-24 00:30:49 +0530

    Time: 36 ms (71.16%), Space: 80.1 MB (19.07%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.java
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.java
@@ -3,36 +3,34 @@ class Solution {
         int wordlen=words[0].length();
         int targetlen=target.length();
         int charfreq[][]= new int[wordlen][26];
-        long dp[][]= new long[ wordlen+1][targetlen+1];
-        for(int i=0;i<wordlen+1;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        for(String word:words){
+          for(String word:words){
             for(int i=0;i<wordlen;i++){
                 int currindx=word.charAt(i)-'a';
                 charfreq[i][currindx]++;
             }
         }
-        return  (int)recur(0,0,charfreq,words,target,dp);
 
-        
-        
-    }
-     public long recur( int wordindx,int targetindx,int charfreq[][],String[] words, String target,long[][] dp) {
-        if(targetindx==target.length()) return 1; // solution find
-             
-        if(wordindx==words[0].length()) return 0;
-        if(target.length()-targetindx > words[0].length()-wordindx)  return 0;
-        if(dp[wordindx][targetindx]!= -1) return dp[wordindx][targetindx];
+        long dp[][]= new long[ wordlen+1][targetlen+1];
+        for(int i=0;i<wordlen+1;i++){
+           dp[i][targetlen]=1;
+        }
+        for(int i=wordlen-1;i>=0;i--){
+            for(int j=targetlen-1;j>=0;j--){
+                  int indx=target.charAt(j)-'a';
+                    int freq= charfreq[i][indx];
 
-        int indx=target.charAt(targetindx)-'a';
-        int freq= charfreq[wordindx][indx];
-
-        long pick=freq*recur(wordindx+1,targetindx+1,charfreq,words,target,dp);
-        long nopick= recur(wordindx+1,targetindx,charfreq,words,target,dp);
+        long pick=freq* dp[i+1][j+1];//recur(wordindx+1,targetindx+1,charfreq,words,target,dp);
+        long nopick= dp[i+1][j];//recur(wordindx+1,targetindx,charfreq,words,target,dp);
         long ans=(pick+nopick)%1000000007;
 
-        dp[wordindx][targetindx]= ans;
-        return ans;
+        dp[i][j]= ans;
+        
 
-}}
+            }
+        }
+        return  (int)dp[0][0];//recur(0,0,charfreq,words,target,dp);
+
+        
+        
+    }}
+    
