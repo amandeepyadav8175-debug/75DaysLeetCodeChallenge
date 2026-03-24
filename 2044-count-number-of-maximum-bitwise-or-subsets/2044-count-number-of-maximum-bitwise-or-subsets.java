13e6eb32e3ac19e3253596c8f185be6cabfc80d4
amandeepyadav8175-debug <amandeepyadav8175@gmail.com>
2026-03-24 08:50:49 +0530

    Time: 9 ms (51.69%), Space: 204.9 MB (5.14%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -3,17 +3,19 @@ class Solution {
         int targetor=0;
         for(int num: nums){
             targetor= targetor| num;
-
         }
-        return recur(0,targetor,0,nums);
+        Integer dp[][]= new Integer[nums.length][targetor+1];//fill either integer value or null
+        return recur(0,targetor,0,nums,dp);
         
     }
-    public int recur(int indx,int targetor,int curror,int[] nums){
+    public int recur(int indx,int targetor,int curror,int[] nums,Integer dp[][]){
         if(indx==nums.length){
             return (curror==targetor)?1:0;
         }
-        int pick= recur( indx +1, targetor, curror|nums[indx], nums);
-         int nopick = recur( indx +1, targetor, curror, nums);
-         return pick+nopick;
+        if( dp[indx][curror]!= null)  return dp[indx][curror];
+        int pick= recur( indx +1, targetor, curror|nums[indx], nums,dp);
+         int nopick = recur( indx +1, targetor, curror, nums,dp);
+         dp[indx][curror]= pick+nopick;
+         return   dp[indx][curror];
     }
 }
