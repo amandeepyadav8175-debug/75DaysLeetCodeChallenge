6629321cce0c79749f10bbbc0bab0adf9b69e640
amandeepyadav8175-debug <amandeepyadav8175@gmail.com>
2026-03-26 00:52:54 +0530

    Time: 2 ms (13.59%), Space: 46.2 MB (90.36%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int dp[][]= new int[prices.length+1][2];//0,1// handles base case prices.length+1 like if(indx==prices.length) return 0; we hold when i==prices+1;// here no need to fill so int dp[]
+         int profit=0;
+         for(int indx=prices.length-1;indx>=0;indx--){
+            for(int canbuy=0;canbuy<=1;canbuy++){
+         
+        if(canbuy==1){
+            int buy= -prices[indx]+ dp[indx+1][0];//recur(indx+1,0,prices,dp);
+            int skip=0+dp[indx+1][1]; // recur(indx+1,1,prices,dp);
+            profit=Math.max(buy,skip);
+        }else{
+            int sell=prices[indx]+  dp[indx+1][1];//recur(indx+1,1,prices,dp);
+            int skip=0+ dp[indx+1][0];//recur(indx+1,0,prices,dp);
+            profit=Math.max(sell,skip);
+        }
+        dp[indx][canbuy] = profit;
+            }}
+      
+        return dp[0][1];//recur(0,1,prices,dp);
+        
+    }}
+    //What is dp[n][0] and dp[n][1] ?
+//   So what is profit after last day?
+
+// No matter what:
+
+// You cannot buy ❌
+// You cannot sell ❌
+
+// 👉 Profit = 0
+// dp[n][0] = 0
+// dp[n][1] = 0
