672a892b114eb6206dd2fdec6a1c1effb80826b5
amandeepyadav8175-debug <amandeepyadav8175@gmail.com>
2026-03-26 00:57:57 +0530

    Time: 1 ms (16.38%), Space: 46.5 MB (56.19%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,23 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int dp[][]= new int[prices.length+1][2];//0,1// handles base case prices.length+1 like if(indx==prices.length) return 0; we hold when i==prices+1;// here no need to fill so int dp[]
+        int next[]= new int[2];//0,1// handles base case prices.length+1 like if(indx==prices.length) return 0; we hold when i==prices+1;// here no need to fill so int dp[]
          int profit=0;
          for(int indx=prices.length-1;indx>=0;indx--){
+            int curr[]= new int[2];
             for(int canbuy=0;canbuy<=1;canbuy++){
          
         if(canbuy==1){
-            int buy= -prices[indx]+ dp[indx+1][0];//recur(indx+1,0,prices,dp);
-            int skip=0+dp[indx+1][1]; // recur(indx+1,1,prices,dp);
+            int buy= -prices[indx]+ next[0];//recur(indx+1,0,prices,dp);
+            int skip=0+next[1]; // recur(indx+1,1,prices,dp);
             profit=Math.max(buy,skip);
         }else{
-            int sell=prices[indx]+  dp[indx+1][1];//recur(indx+1,1,prices,dp);
-            int skip=0+ dp[indx+1][0];//recur(indx+1,0,prices,dp);
+            int sell=prices[indx]+ next[1];//recur(indx+1,1,prices,dp);
+            int skip=0+ next[0];//recur(indx+1,0,prices,dp);
             profit=Math.max(sell,skip);
         }
-        dp[indx][canbuy] = profit;
-            }}
+        curr[canbuy] = profit;
+            }
+            next=curr;
+            }
       
-        return dp[0][1];//recur(0,1,prices,dp);
+        return next[1];//recur(0,1,prices,dp);
         
     }}
     //What is dp[n][0] and dp[n][1] ?
