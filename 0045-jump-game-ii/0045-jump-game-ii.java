a7baf7e7dfd7e2b9b2709e8d8b733f85ef2ddfa7
amandeepyadav8175-debug <amandeepyadav8175@gmail.com>
2026-03-26 13:07:37 +0530

    Time: 1 ms (99.65%), Space: 47.1 MB (90.94%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,23 +1,20 @@
 class Solution {
     public int jump(int[] nums) {
-        int n= nums.length;
-        Integer[] dp= new Integer[n];
-        return recur(0,nums,dp);
-        
-    }
-    public int recur(int indx,int[] nums,  Integer[] dp){
-        if(indx>=nums.length-1) return 0;// we have to stop when rech last indx and further 
-       // indx
-       if(dp[indx]!= null) return dp[indx];
-        int minjump= Integer.MAX_VALUE;
+        int n=nums.length;
+        int jumps=0;
+        int currend=0;
+        int farthest=0;
+        for(int i=0;i<n-1;i++){
+            farthest= Math.max(farthest,i+nums[i]);
 
-        for(int steps=1;steps<=nums[indx];steps++){
-            int pick= recur(indx+steps,nums,dp);
-            if(pick!=Integer.MAX_VALUE){
-            minjump=Math.min(minjump,1+pick);//First store recursion result, then check, then add 1
 
-        }}
-        dp[indx]= minjump;
-        return minjump;
+            if(i==currend){
+                jumps++;
+                currend=farthest;
+
+            }
+        }
+        return jumps;
+      
     }
 }
