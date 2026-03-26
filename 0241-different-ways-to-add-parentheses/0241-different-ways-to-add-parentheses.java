ac9d6cb14de886391066ad57ed0fc75b248f7727
amandeepyadav8175-debug <amandeepyadav8175@gmail.com>
2026-03-26 11:36:01 +0530

    Time: 1 ms (99.8%), Space: 43.8 MB (70.75%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,12 +1,14 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String expression) {
-        
         int n=  expression.length();
-        return recur( expression, 0,n-1);
+        List<Integer>[][] dp= new ArrayList[n][n];
+        
+        return recur( expression, 0,n-1,dp);
         
     }
-     public List<Integer> recur(String expression,int start,int end){
+     public List<Integer> recur(String expression,int start,int end,  List<Integer>[][] dp){
          List<Integer> res= new ArrayList<>();
+         if(dp[start][end]!= null) return dp[start][end];
 
         if(start==end){// means only single digit [5]
         int num=expression.charAt(start)-'0'; //convert string to int
@@ -15,15 +17,15 @@ class Solution {
 
         }
         if(end-start==1 && Character.isDigit(expression.charAt(start))){// means two elemnt
-         int num=Integer.parseInt( expression.substring(start,end+1));
+         int num=Integer.parseInt( expression.substring(start,end+1));////convert string to int
            res.add(num);
         return res;
         }
         // split
         for(int i=start;i<=end;i++){
             if( Character.isDigit(expression.charAt(i))) continue;
-            List<Integer> left= recur(expression,start,i-1);
-            List<Integer> right= recur(expression,i+1,end);
+            List<Integer> left= recur(expression,start,i-1,dp);
+            List<Integer> right= recur(expression,i+1,end,dp);
             int op=expression.charAt(i);
 
             for(int l: left){
@@ -39,7 +41,8 @@ class Solution {
                 }
             }
         }
-        return res;
+       dp[start][end]= res;
+       return res;
 
      }
 
