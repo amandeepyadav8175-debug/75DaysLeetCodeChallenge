3145def6a6800b4eac1d6f31cb5a087e788738d2
Amandeep yadav <amandeepyadav8175@gmail.com>
2026-06-29 23:14:42 +0530

    Time: 4 ms (89.95%), Space: 47.8 MB (5.88%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        int n= s.length();
+        if(n<k) return false;
+        if(n==k) return true;
+        int count[]= new int[26];
+
+        for(int i=0;i<n;i++){
+            count[s.charAt(i)- 'a']++;
+        }
+        int oddcount=0;// not char count
+        //  find odd count;
+        for(int i=0;i<26;i++){
+            if(count[i]%2!=0) oddcount++;
+        }
+        return (oddcount<= k);
+        
+    }
+}
