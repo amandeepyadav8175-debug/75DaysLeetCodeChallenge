17cf181e361c524c8b64b7ec731732757d99e156
Amandeep yadav <amandeepyadav8175@gmail.com>
2026-07-03 12:26:43 +0530

    Time: 1 ms (99.98%), Space: 47.5 MB (22.05%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,17 +1,20 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        HashSet<Integer> set = new HashSet<>();
+       // HashSet<Integer> set = new HashSet<>();
         int n= grid.length;
         int sq=n*n;
         int ans[]= new int[2];
         int currsum=0;
+        int set[]= new int[sq+1];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(set.contains(grid[i][j])){
+               // if(set.contains(grid[i][j])){
+               if(set[grid[i][j]]!=0){
                     ans[0]= grid[i][j];
 
                 }else{
-                    set.add(grid[i][j]);
+                    //set.add(grid[i][j]);
+                    set[grid[i][j]]=1;
                     currsum+=grid[i][j];
                 }
             }
